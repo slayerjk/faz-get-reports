c10f2fa375820bc0e25136c74cc8d10c24ef48de
slayerjk <slayer.jk@mail.ru>
2024-10-29 09:48:46 +0500

    *refactoring

--- go.mod
+++ go.mod
@@ -4,4 +4,11 @@ go 1.22.3
 
 require github.com/go-ldap/ldap v3.0.3+incompatible // direct
 
-require gopkg.in/asn1-ber.v1 v1.0.0-20181015200546-f715ec2f112d // indirect
+require github.com/ncruces/go-sqlite3 v0.20.0
+
+require (
+	github.com/ncruces/julianday v1.0.0 // indirect
+	github.com/tetratelabs/wazero v1.8.1 // indirect
+	golang.org/x/sys v0.26.0 // indirect
+	gopkg.in/asn1-ber.v1 v1.0.0-20181015200546-f715ec2f112d // indirect
+)
--- internal/dboperations/dboperations.go
+++ internal/dboperations/dboperations.go
@@ -1,1 +1,41 @@
 package dboperations
+
+import (
+	// sqllite support
+	"database/sql"
+	"fmt"
+
+	_ "github.com/ncruces/go-sqlite3/driver"
+	_ "github.com/ncruces/go-sqlite3/embed"
+)
+
+// get list of unprocessed values in db
+func GetUnprocessedDbValues(dbFile, dbTable, dbValueColumn, dbProcessedColumn string) ([]string, error) {
+	result := make([]string, 0)
+
+	// open db file
+	db, err := sql.Open("sqlite3", "file:"+dbFile)
+	if err != nil {
+		return nil, fmt.Errorf("failed to open db file(%s):\n\t%v", dbFile, err)
+	}
+	defer db.Close()
+
+	// get select result of unprocessed values
+	query := fmt.Sprintf("SELECT %s FROM %s WHERE %s IS NULL", dbValueColumn, dbTable, dbProcessedColumn)
+	rows, err := db.Query(query)
+	if err != nil {
+		return nil, fmt.Errorf("failed to make select of unprocessed values:\n\t%v", err)
+	}
+
+	for rows.Next() {
+		var value string
+
+		if err := rows.Scan(&value); err != nil {
+			return nil, fmt.Errorf("failed to scan rows of select of unprocessed values:\n\t%v", err)
+		}
+		result = append(result, value)
+	}
+
+	db.Close()
+	return result, nil
+}
